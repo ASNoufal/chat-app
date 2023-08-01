d0708ee5e43726be90c3dcd29819648f6b57b754
Naufal As <naufal202@gmail.com>
2023-08-01 10:25:49 +0530

    firebase updated

--- lib/screen/auth.dart
+++ lib/screen/auth.dart
@@ -8,7 +8,20 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
   bool _isLogin = true;
+  String emailid = '';
+  String passid = '';
+  void submit() {
+    final validate = _form.currentState!.validate();
+
+    if (validate) {
+      _form.currentState!.save();
+      print(emailid);
+      print(passid);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,37 +41,68 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Form(
+                          key: _form,
                           child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text('Email Id'),
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                            autocorrect: false,
-                            textCapitalization: TextCapitalization.none,
-                          ),
-                          TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text('Password'),
-                            ),
-                            obscureText: true,
-                          ),
-                          ElevatedButton(
-                              onPressed: () {},
-                              child: Text(_isLogin ? "Login" : "Sign up")),
-                          TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isLogin = !_isLogin;
-                                });
-                              },
-                              child: Text(_isLogin
-                                  ? "Create an account"
-                                  : "Already have Existing account"))
-                        ],
-                      ))),
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().isEmpty ||
+                                      !value.contains("@")) {
+                                    return "UserName is incorrect";
+                                  }
+
+                                  return null;
+                                },
+                                onSaved: (newValue) {
+                                  emailid = newValue!;
+                                },
+                                decoration: const InputDecoration(
+                                  label: Text('Email Id'),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                autocorrect: false,
+                                textCapitalization: TextCapitalization.none,
+                              ),
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.trim().isEmpty ||
+                                      value.length < 6) {
+                                    return "Password is Too short";
+                                  }
+                                  return null;
+                                },
+                                onSaved: (newValue) {
+                                  passid = newValue!;
+                                },
+                                decoration: const InputDecoration(
+                                  label: Text('Password'),
+                                ),
+                                obscureText: true,
+                              ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              ElevatedButton(
+                                  onPressed: submit,
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Theme.of(context)
+                                          .colorScheme
+                                          .primaryContainer),
+                                  child: Text(_isLogin ? "Login" : "Sign up")),
+                              TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isLogin = !_isLogin;
+                                    });
+                                  },
+                                  child: Text(_isLogin
+                                      ? "Create an account"
+                                      : "Already have Existing account"))
+                            ],
+                          ))),
                 ),
               ),
             ],
