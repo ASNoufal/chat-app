94ca96866d85035dd3dcf202c4cc44d587b38091
Naufal As <naufal202@gmail.com>
2023-08-05 16:51:56 +0530

    chat are added to the chat box

--- lib/widget/newmessage.dart
+++ lib/widget/newmessage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatelessWidget {
@@ -5,8 +6,32 @@ class NewMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("no messages"),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: false)
+            .snapshots(),
+        builder: ((context, chatsnapshot) {
+          if (chatsnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!chatsnapshot.hasData || chatsnapshot.data!.docs.isEmpty) {
+            return const Center(
+              child: Text("no messages"),
+            );
+          }
+
+          if (chatsnapshot.hasError) {
+            return const Center(child: Text("found Error"));
+          }
+          final chatsnap = chatsnapshot.data!.docs;
+          return ListView.builder(
+              itemCount: chatsnap.length,
+              itemBuilder: (context, index) {
+                return Text(chatsnap[index].data()['text']);
+              });
+        }));
   }
 }
